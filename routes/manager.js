c3b9ac4e2af46e097efc0662642a63b7a555cfa4
Jose David <david_gh86@hotmail.es>
2021-05-16 19:15:03 +0200

    first commit

---
+++ routes/manager.js
@@ -0,0 +1,118 @@
+var express = require('express');
+const { exec } = require("child_process");
+var router = express.Router();
+
+function getContainerName(owner) {
+  let containerName
+  if ("david"==owner){
+    containerName = "contenedor_divergencias_david"
+  }else if("alberto"==owner){
+    containerName = "contenedor_divergencias_alberto"
+  }
+  return containerName;
+}
+
+/* Shows status */
+router.post('', function(req, res, next) {
+  let owner = req.body.owner;
+  getStatusAndRedirect(owner, res);
+});
+
+/* stop and show status */
+router.post('/stop', function(req, res, next) {
+  let owner = req.body.owner;
+  let containerName = getContainerName(owner)
+  let command = `docker stop ${containerName}`;
+  executeCommand(command, owner, res, getStatusAndRedirect);
+});
+
+/* start and show status */
+router.post('/start', function(req, res, next) {
+  let owner = req.body.owner;
+  let containerName = getContainerName(owner)
+  let command = `docker start ${containerName}`;
+  executeCommand(command, owner, res, getStatusAndRedirect);
+});
+
+/* restart and show status */
+router.post('/restart', function(req, res, next) {
+  let owner = req.body.owner;
+  let containerName = getContainerName(owner)
+  let command = `docker restart ${containerName}`;
+  executeCommand(command, owner, res, getStatusAndRedirect);
+});
+
+/* restart and show status */
+router.post('/logs', function(req, res, next) {
+  let owner = req.body.owner;
+  let containerName = getContainerName(owner)
+  let command = `docker logs ${containerName} 2>&1 | cat`;
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      res.render('../views/error', { error });
+    } else if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      res.render('../views/error', {
+        error: {
+          message: stderr
+        }
+      });
+    } else {
+      res.render('../views/logs', {owner: owner, log: stdout})
+    }
+  });
+});
+
+function executeCommand(command, owner, res, callback) {
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      res.render('../views/error', { error });
+    } else if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      res.render('../views/error', {
+        error: {
+          message: stderr
+        }
+      });
+    } else {
+      callback(owner, res);
+    }
+  });
+}
+
+function getStatusAndRedirect(owner, res) {
+  let containerName = getContainerName(owner);
+  let command = `docker ps --filter \"name=${containerName}\" --format \"{{.Status}}\"`;
+
+  exec(command, (error, stdout, stderr) => {
+    let status = "Status: ";
+    if (error) {
+      console.log(`error: ${error.message}`);
+      res.render('../views/error', { error });
+    } else if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      res.render('../views/error', {
+        error: {
+          message: stderr
+        }
+      });
+    } else { 
+      if (!stdout) {
+        status += "Caido";
+      } else {
+        status += stdout;
+      }
+      res.render('../views/status',
+        {
+          status: status,
+          owner: owner
+        });
+      }
+  });
+}
+
+module.exports = router;
+
+
