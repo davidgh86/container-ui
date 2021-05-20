ac74c954df3dee68ea3211000cc91cc4636e16bc
Jose David <david_gh86@hotmail.es>
2021-05-20 22:05:33 +0200

    containers

---
+++ routes/containerList.js
@@ -0,0 +1,24 @@
+module.exports = Object.freeze({
+    CONTAINERS: [
+        {
+            key: "davidMACD",
+            description: "David MACD",
+            container: "contenedor_divergencias_david"
+        },
+        {
+            key: "davidRSI",
+            description: "David RSI",
+            container: "contenedor_divergencias_david_rsi"
+        },
+        {
+            key: "albertoMACD",
+            description: "Alberto MACD",
+            container: "contenedor_divergencias_alberto"
+        },
+        {
+            key: "albertoRSI",
+            description: "Alberto RSI",
+            container: "contenedor_divergencias_alberto_rsi"
+        }
+    ]
+})
