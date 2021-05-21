6115a865ae080ab7e596d7d509f03e452ec6ebf4
Jose David <david_gh86@hotmail.es>
2021-05-21 18:53:31 +0200

    service commands

--- serverprocess.sh
+++ serverprocess.sh
@@ -3,3 +3,4 @@ sudo systemctl stop gestion_contenedores
 sudo systemctl start gestion_contenedores
 sudo systemctl daemon-reload gestion_contenedores
 sudo nano /lib/systemd/system/gestion_contenedores.service
+logs PATH /home/david/container-ui/public/logs
